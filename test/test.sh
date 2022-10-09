3e5cf03bc65840cfcc86eb6d1340027dae89af5e
luomgf <luomgf@163.com>
2022-10-09 17:12:33 +0800

    添加测试数据

---
+++ test/test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ 
+../csv2ECharts.sh  ../test/data1.csv  > ~/Desktop/data1.html
+../csv2ECharts.sh  ../test/free_mon_202209.csv  > ~/Desktop/free_mon_202209.html
+
+../csv2ECharts.sh  ../test/ssar.csv  > ~/Desktop/ssar.html
